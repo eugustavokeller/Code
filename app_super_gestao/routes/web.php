56b6646b43794a39bf1df3ca644c5abd1eb5a909
Gustavo Keller <gustavo.lealtec@gmail.com>
2022-07-30 20:56:13 -0300

    Redirecionamento de rotas
    Através da Função ''Callback'' da Rota criada ou especificando diretamente com "Route::redirect".

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -2,22 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-/*
--------------
+/*-------------
 NAVIGATION MENU
--------------
-*/
+-------------*/
 
 Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobre-nos');
@@ -27,23 +14,30 @@ Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::get('/login', function () {
     return 'Login';
 })->name('site.login');
-/*
--------------
+
+/*-------------
 ROTAS IMPLEMENTADAS, PRODUTOS, FORNECEDORES, CLIENTES
--------------
-*/
+-------------*/
+
 Route::prefix('/app')->group(function () {
-    Route::get('/clientes', function () { return 'Clientes';});
-    Route::get('/fornecedores', function () { return 'Fornecedores';});
-    Route::get('/produtos', function () { return 'Produtos';});
+    Route::get('/clientes', function () {
+        return 'Clientes';
+    });
+    Route::get('/fornecedores', function () {
+        return 'Fornecedores';
+    });
+    Route::get('/produtos', function () {
+        return 'Produtos';
+    });
 });
 
+Route::get('/rota1', function () {
+    echo 'Rota 1';
+})->name('site.rota1');
 
-//verbo http - 
 
-// get
-// post
-// put
-// patch
-// delete
-// options
+Route::get('/rota2', function () {
+    return redirect(route('site.rota1'));
+})->name('site.rota2'); // Redirecionamento na funcão Callback...
+
+// Route::redirect('/rota2', 'rota1');  // Redirecionamento na Route
