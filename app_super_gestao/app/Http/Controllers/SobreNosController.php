086cadcdef6f32a93515504c950a91ee22277317
Gustavo Keller <gustavo.lealtec@gmail.com>
2022-07-30 21:20:42 -0300

    Parametros da rota para o controlador
    Parametros da rota para o controlador

--- app_super_gestao/app/Http/Controllers/SobreNosController.php
+++ app_super_gestao/app/Http/Controllers/SobreNosController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class SobreNosController extends Controller
 {
-    public function sobreNos() {
+    public function sobreNos()
+    {
         return view('site.sobreNos');
     }
 }
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -31,16 +31,8 @@ Route::prefix('/app')->group(function () {
     });
 });
 
-Route::get('/rota1', function () {
-    echo 'Rota 1';
-})->name('site.rota1');
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
 
-
-Route::get('/rota2', function () {
-    return redirect(route('site.rota1'));
-})->name('site.rota2'); // Redirecionamento na funcão Callback...
-
-// Route::redirect('/rota2', 'rota1');  // Redirecionamento na Route
 
 Route::fallback(function() {
     echo 'Página não encontrada <a href="'.route('site.index').'">clique aqui</a> para voltar ao menu.';
