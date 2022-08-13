2b024057ecfc193db383ea41da96a9a706808992
Gustavo Keller <gustavo.lealtec@gmail.com>
2022-08-13 13:57:12 -0300

    {{ asset() }}
    asset aponta para pasta public default.
    referenciando link externo CSS.

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -8,7 +8,7 @@ NAVIGATION MENU
 -------------*/
 
 Route::get('/', 'PrincipalController@principal')->name('site.index');
-Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobre-nos');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::get('/login', function () { return 'Login';})->name('site.login');
 
