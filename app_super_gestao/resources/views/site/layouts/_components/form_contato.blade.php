0b284da4664a44e06ea3689293631ba49540521f
Gustavo Keller <85264199+eugustavokeller@users.noreply.github.com>
2022-08-30 16:47:43 -0300

    Mensagem de erro melhorada
    Mensagem de validação de campo com erro costumizado.

--- app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
+++ app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
@@ -2,10 +2,25 @@
 <form action={{ route('site.contato') }} method="post">
     @csrf
     <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{ $classe }}">
+    @if($errors->has('nome'))
+        <div style="background:red">
+        {{ $errors->first('nome') }}
+        </div>
+    @endif
     <br>
     <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{ $classe }}">
+    @if ($errors->has('telefone'))
+        <div style="background:red">
+        {{ $errors->first('telefone') }}
+        </div>
+    @endif
     <br>
     <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{ $classe }}">
+    @if ($errors->has('email'))
+        <div style="background:red">
+        {{ $errors->first('email') }}
+        </div>
+    @endif
     <br>
 
     <select name="motivo_contatos_id" class="{{ $classe }}">
@@ -16,16 +31,15 @@
                 {{ $motivo_contato->motivo_contato }}</option>
         @endforeach
     </select>
+    <div style="background:red">
+    {{ $errors->has('motivo_contatos_id')  ?  $errors->first('motivo_contatos_id') : '' }} 
+    </div>
     <br>
     <textarea name="mensagem" class="{{ $classe }}">{{ old('mensagem') != '' ? old('mensagem') : 'Preencha aqui a sua mensagem' }}</textarea>
+    <div style="background:red">
+    {{ $errors->has('mensagem') ? $errors->first('mensagem') : '' }}
+    </div>
     <br>
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
-@if ($errors->any())
-    <div style="position:absolute; top:0px; left:0px; width:100%; background:red">
-        @foreach ($errors->all() as $erro)
-            {{ $erro }}
-            <br>
-        @endforeach
-    </div>
-@endif
+
