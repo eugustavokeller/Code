927379473388ff3a65e0911d11f8b07685a9a425
Gustavo Keller <85264199+eugustavokeller@users.noreply.github.com>
2022-08-31 08:27:50 -0300

    Middleware

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -2,28 +2,37 @@
 
 use App\Http\Controllers\FornecedorController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Middleware\LogAcessoMiddleware;
 /*-------------
 NAVIGATION MENU
 -------------*/
 
-Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::middleware(LogAcessoMiddleware::class)
+    ->get('/', 'PrincipalController@principal')
+    ->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
-Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato')
+    ->middleware(LogAcessoMiddleware::class);
 Route::post('/contato', 'ContatoController@contato')->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
-Route::get('/login', function () { return 'Login';})->name('site.login');
+Route::get('/login', function () {
+    return 'Login';
+})->name('site.login');
 
 Route::prefix('/app')->group(function () {
-    Route::get('/clientes', function () { return 'Clientes'; })->name('app.clientes');
+    Route::get('/clientes', function () {
+        return 'Clientes';
+    })->name('app.clientes');
     Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
-    Route::get('/produtos', function () { return 'Produtos'; })->name('app.produtos');
+    Route::get('/produtos', function () {
+        return 'Produtos';
+    })->name('app.produtos');
 });
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
 
 Route::get('/notfound', 'NotFoundController@notfound')->name('notfound');
 
-Route::fallback(function(){
+Route::fallback(function () {
     return view('site.notfound', ['titulo' => 'Nao Encontrada']);
 });
