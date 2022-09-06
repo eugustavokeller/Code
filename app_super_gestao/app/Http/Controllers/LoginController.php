74b612164d749f16938abf195c8800997b981f58
Gustavo Keller <85264199+eugustavokeller@users.noreply.github.com>
2022-09-06 14:29:37 -0300

    Validando erros
    Validação de erros na autenticação de usuário cadastrado no bando de dados.

--- app_super_gestao/app/Http/Controllers/LoginController.php
+++ app_super_gestao/app/Http/Controllers/LoginController.php
@@ -7,8 +7,15 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function index() {
-        return view('site.login', ['titulo' => 'Login']);
+    public function index(Request $request) {
+
+        $erro = '';
+        
+        if($request->get('erro') == 1) {
+            $erro = 'Usuario ou senha inexistentes';
+        };
+
+        return view('site.login', ['titulo' => 'Login', 'erro' => $erro]);
     }
 
     public function autenticar(Request $request) {
@@ -45,7 +52,7 @@ class LoginController extends Controller
         if(isset($usuario->name)) {
             echo 'Usuario existe';
         } else {
-            echo 'Usuario não existe';
+            return redirect()->route('site.login', ['erro' => 1]);
         }
         
     }
