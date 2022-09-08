e1e53ba16709d362d0b008b8017b9693e813f8c1
Gustavo Keller <85264199+eugustavokeller@users.noreply.github.com>
2022-09-08 11:12:01 -0300

    Update fornecedores
    Update fornecedores

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -3,10 +3,54 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Fornecedor;
 
 class FornecedorController extends Controller
 {
     public function index() {
-        return view('app.fornecedor');
+        return view('app.fornecedor.index');
+    }
+
+    public function listar(Request $request) {
+
+        $fornecedores = Fornecedor::where('nome', 'like', '%'.$request->input('nome').'%')
+            ->where('site', 'like', '%'.$request->input('site'))
+            ->where('uf', 'like', '%'.$request->input('uf'))
+            ->where('email', 'like', '%'.$request->input('email'))
+            ->get();
+        
+            return view('app.fornecedor.listar', ['fornecedores' => $fornecedores]);
+    }
+
+    public function adicionar(Request $request) {
+
+        $msg = '';
+
+        if($request->input('_token') != '') {
+            //validacao
+            $regras = [
+                'nome' => 'required|min:3|max:50',
+                'site' => 'required',
+                'uf' => 'required|min:2|max:2',
+                'email' => 'email',
+            ];
+
+            $feedback = [
+                'required' => 'O campo :attribute deve ser preenchido',
+                'nome.min' => 'O campo nome deve ter no mínimo 3 caracteres',
+                'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+                'uf.min' => 'O campo UF deve ter 2 caracteres',
+                'uf.max' => 'O campo UF deve ter 2 caracteres',
+                'email.email' => 'Insira um email valido: exemplo@exemplo.com',
+             ];
+
+             $request->validate($regras, $feedback);
+
+             $fornecedor = new Fornecedor();
+             $fornecedor->create($request->all());
+
+             $msg = 'Cadastro realizado com sucesso!';
+        }
+        return view('app.fornecedor.adicionar', ['msg' => $msg]);
     }
 }
