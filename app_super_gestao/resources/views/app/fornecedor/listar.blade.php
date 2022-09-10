7275e738c99d32e267c49d02c28d2aa8cb93288f
Gustavo Keller <gustavo.lealtec@gmail.com>
2022-09-09 21:30:52 -0300

    Paginate of registers
    Paginate of registers.

--- app_super_gestao/resources/views/app/fornecedor/listar.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/listar.blade.php
@@ -3,20 +3,17 @@
 @section('titulo', 'Fornecedor')
 
 @section('conteudo')
-
     <div class="conteudo-pagina">
 
         <div class="titulo-pagina-2">
             <p>Fornecedor - Listar</p>
         </div>
-
         <div class="menu">
             <ul>
                 <li><a href="{{ route('app.fornecedor.adicionar') }}">Novo</a></li>
                 <li><a href="{{ route('app.fornecedor') }}">Consulta</a></li>
             </ul>
         </div>
-
         <div class="informacao-pagina">
             <div style="width: 90%; margin-left: auto; margin-right: auto">
                 <table style="margin-top: 60px" border="1" width="100%">
@@ -30,7 +27,6 @@
                             <th>Editar</th>
                         </tr>
                     </thead>
-
                     <tbody>
                         @foreach ($fornecedores as $fornecedor)
                             <tr>
@@ -44,9 +40,8 @@
                         @endforeach
                     </tbody>
                 </table>
+                {{ $fornecedores->appends($request)->links() }}
             </div>
         </div>
     </div>
-
-
 @endsection
