33287a9d506551a4df55b9f83e61b79eb092dcbb
Gustavo Keller <85264199+eugustavokeller@users.noreply.github.com>
2022-09-14 15:06:33 -0300

    Method index de resource

---
+++ app_super_gestao/resources/views/app/produto/index.blade.php
@@ -0,0 +1,51 @@
+@extends('app.layouts.basico')
+
+@section('titulo', 'Fornecedor')
+
+@section('conteudo')
+    <div class="conteudo-pagina">
+
+        <div class="titulo-pagina-2">
+            <p>Listagem de Produtos</p>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="">Novo</a></li>
+                <li><a href="{{ route('produto.index') }}">Consulta</a></li>
+            </ul>
+        </div>
+        <div class="informacao-pagina">
+            <div style="width: 90%; margin-left: auto; margin-right: auto">
+                <table style="margin-top: 60px" border="1" width="100%">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Descrição</th>
+                            <th>Peso</th>
+                            <th>Unidade</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($produtos as $produto)
+                            <tr>
+                                <td>{{ $produto->nome }}</td>
+                                <td>{{ $produto->descricao }}</td>
+                                <td>{{ $produto->peso }}</td>
+                                <td>{{ $produto->unidade_id }}</td>
+                                <td><a href="">Excluir</a></td>
+                                <td><a href="">Editar</a></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                {{ $produtos->appends($request)->links() }}
+                <br>
+                Lista do registro {{ $produtos->firstItem()}} ao {{ $produtos->lastItem()}}
+                <br>
+                {{ $produtos->count() }} registros de {{ $produtos->total() }}.
+            </div>
+        </div>
+    </div>
+@endsection
