68fde9dffdb38fbb002ded0135d3bcd2b8af8d25
Gustavo Keller <85264199+eugustavokeller@users.noreply.github.com>
2022-09-19 14:28:51 -0300

    View produto
    Visualização por ID.

--- app_super_gestao/app/Http/Controllers/ProdutoController.php
+++ app_super_gestao/app/Http/Controllers/ProdutoController.php
@@ -39,7 +39,25 @@ class ProdutoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $regras = [
+            'nome' => 'required|min:3|max:40',
+            'descricao' => 'required|min:3|max:2000',
+            'peso' => 'required|integer',
+            'unidade_id' => 'exists:unidades,id'
+        ];
+        $feedback = [
+            'required' => 'O campo :attribute é obrigarório!',
+            'nome.min' => 'O campo :attribute deve ter no minimo 3 caracteres.',
+            'descricao.min' => 'O campo :attribute deve ter no minimo 3 caracteres.',
+            'nome.max' => 'O campo :attribute deve ter no maximo 3 caracteres.',
+            'descricao.max' => 'O campo :attribute deve ter no maximo 3 caracteres.',
+            'peso.integer' => 'O campo peso deve ser um inteiro',
+            'unidade_id.exists' => 'O campo unidade deve ser selecionado.',
+        ];
+        $request->validate($regras, $feedback);
+
+        Produto::create($request->all());
+        return redirect()->route('produto.index');
     }
 
     /**
@@ -50,7 +68,7 @@ class ProdutoController extends Controller
      */
     public function show(Produto $produto)
     {
-        //
+        return view('app.produto.show', ['produto' => $produto]);
     }
 
     /**
--- app_super_gestao/resources/views/app/produto/index.blade.php
+++ app_super_gestao/resources/views/app/produto/index.blade.php
@@ -25,6 +25,7 @@
                             <th>Unidade</th>
                             <th></th>
                             <th></th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -34,6 +35,7 @@
                                 <td>{{ $produto->descricao }}</td>
                                 <td>{{ $produto->peso }}</td>
                                 <td>{{ $produto->unidade_id }}</td>
+                                <td><a href="{{ route('produto.show', ['produto' => $produto->id]) }}">Visualizar</a></td>
                                 <td><a href="">Excluir</a></td>
                                 <td><a href="">Editar</a></td>
                             </tr>
