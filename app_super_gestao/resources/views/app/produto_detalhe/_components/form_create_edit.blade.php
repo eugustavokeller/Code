6dc9a0f2fc1e12abb8617f55d6671c756ac5f588
Gustavo Keller <gustavo.lealtec@gmail.com>
2022-09-23 21:29:38 -0300

    Produtos detalhes

---
+++ app_super_gestao/resources/views/app/produto_detalhe/_components/form_create_edit.blade.php
@@ -0,0 +1,34 @@
+@if (isset($produto_detalhe->id))
+    <form method="post" action="{{ route('produto_detalhe.update', ['produto_detalhe' => $produto_detalhe->id]) }}">
+        @csrf
+        @method('PUT')
+@else
+    <form method="post" action="{{ route('produto-detalhe.store') }}">
+        @csrf
+@endif
+        <input type="text" value="{{ $produto_detalhe->produto_id ?? old('produto_id') }}" name="produto_id" placeholder="ID Produto" class="borda-preta">
+        {{ $errors->has('produto_id') ? $errors->first('produto_id') : '' }}
+
+        <input type="text" value="{{ $produto_detalhe->comprimento ?? old('comprimento') }}" name="comprimento" placeholder="Comprimento"
+            class="borda-preta">
+        {{ $errors->has('comprimento') ? $errors->first('comprimento') : '' }}
+
+        <input type="text" value="{{ $produto_detalhe->largura ?? old('largura') }}" name="largura" placeholder="Largura" class="borda-preta">
+        {{ $errors->has('largura') ? $errors->first('largura') : '' }}
+
+        <input type="text" value="{{ $produto_detalhe->altura ?? old('altura') }}" name="altura" placeholder="Altura" class="borda-preta">
+        {{ $errors->has('altura') ? $errors->first('altura') : '' }}
+
+        <select name="unidade_id">
+            <option> -- Selecione a Unidade de Medida -- </option>
+
+            @foreach ($unidades as $unidade)
+                <option value="{{ $unidade->id }}"
+                    {{ ($produto_detalhe->unidade->id ?? old('unidade_id')) == $unidade->id ? 'selected' : '' }}>
+                    {{ $unidade->descricao }}</option>
+            @endforeach
+        </select>
+        {{ $errors->has('unidade_id') ? $errors->first('unidade_id') : '' }}
+
+        <button type="submit" class="borda-preta">Cadastrar</button>
+    </form>
