b582247029269ac7d32ca839378468c212d13e15
Gustavo Keller <gustavo.lealtec@gmail.com>
2022-09-29 21:25:58 -0300

    Update detalhes do produto
    hasOne -> Tem um..
    belongsTo -> Pertence a..

--- app_super_gestao/app/Produto.php
+++ app_super_gestao/app/Produto.php
@@ -12,4 +12,13 @@ class Produto extends Model
         'peso',
         'unidade_id',
     ];
+
+    public function produtoDetalhe() {
+        return $this->hasOne('App\ProdutoDetalhe');
+
+        // Produto tem 1 produtoDetalhe
+
+        // 1 registro relacionado em produto_detalhes (fk) -> produto_id
+        // produtos (pk) -> id
+    }
 }
