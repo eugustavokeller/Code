e6f9e174a9acb470eaf11cac5d89686579bb546c
Gustavo Keller <85264199+eugustavokeller@users.noreply.github.com>
2022-10-04 13:13:08 -0300

    Update

--- app_super_gestao/app/Http/Controllers/ProdutoDetalheController.php
+++ app_super_gestao/app/Http/Controllers/ProdutoDetalheController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\ItemDetalhe;
 use App\Produto;
 use App\ProdutoDetalhe;
 use App\Unidade;
@@ -55,11 +56,12 @@ class ProdutoDetalheController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  Interger $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(ProdutoDetalhe $produtoDetalhe)
+    public function edit($id)
     {
+        $produtoDetalhe = ItemDetalhe::find($id);
         $unidades = Unidade::all();
         return view('app.produto_detalhe.edit', ['produto_detalhe' => $produtoDetalhe, 'unidades' => $unidades]);
     }
